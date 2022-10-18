3e953c0fbac54cb4586a790915c49fff98b87b43
Aashish Jangam <aashish.jangam55@gmail.com>
2022-10-18 14:14:25 +0545

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,23 @@ import reportWebVitals from './reportWebVitals';
 import Widget from './ChampselectInlineWidget/ChampselectInlineWidget';
 
 const champselectInlineWidget = document.getElementById('champselect-inline-widget')
-ReactDOM.render(
-  <React.StrictMode>
-    <Widget appName={champselectInlineWidget?.getAttribute('data-slug') || ''} />
-  </React.StrictMode>,
-  champselectInlineWidget
-);
-
 const root = document.getElementById('root')
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  root
-);
+
+if (champselectInlineWidget) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Widget appName={champselectInlineWidget?.getAttribute('data-slug') || ''} />
+    </React.StrictMode>,
+    champselectInlineWidget
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
